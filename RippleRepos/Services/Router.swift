3470d2902971ab939f32051823a2171f69e639b5
Noor El-Din Walid <noorwalid72@yahoo.com>
2022-04-23 21:33:52 +0200

    Cleaning.

--- RippleRepos/Services/Router.swift
+++ RippleRepos/Services/Router.swift
@@ -28,14 +28,6 @@ enum Router: URLRequestConvertible {
         }
     }
     
-    //The headers here are for the Github authentication using OAuth2.0, I created an app on Github
-        //with clientID and client secret codes, and it's embedded here in the headers of the call.
-    var headers: [String:String] {
-        switch self {
-        case .byRepoName:
-            return ["client_id":"775369ca2e8f906a28b4", "client_secret":"d3c42f876fec8d23eb7cb72acb910841dc43fba6"]
-        }
-    }
     
     var parameters: [String:Any]? {
         switch self {
@@ -53,7 +45,7 @@ enum Router: URLRequestConvertible {
         case .byRepoName(_):
             request = try URLEncoding.default.encode(request, with: parameters)
         }
-        request.headers = HTTPHeaders(headers)
+//        request.headers = HTTPHeaders(headers)
         return request
     }
 }
